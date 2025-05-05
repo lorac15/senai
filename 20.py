9c7bf18665ecdba3c7d64baee9341ec46bdfa75c
carol do Dk <caroline_ribeiro2@estudante.sesisenai.org.br>
2025-05-05 10:08:46 -0300

    update

---
+++ 20.py
@@ -0,0 +1,6 @@
+maior = float (input("diagonal maior: "))
+menor = float (input("diagonal menor: "))
+
+area = (maior * menor) / 2
+
+print ("area: ", area)
---
+++ 25.py
@@ -0,0 +1,6 @@
+cateto1 = float (input("cateto"))
+cateto2 = float (input("cateto"))
+
+hipotenusa = (cateto1 ** 2) + (cateto2 ** 2)
+
+print (" hipotenusa: ", hipotenusa)
