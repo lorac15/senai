c887ab11afbf64d1d7b1d276b22d1518cc60c956
carol do Dk <caroline_ribeiro2@estudante.sesisenai.org.br>
2025-05-12 08:54:09 -0300

    update

--- 2.2.py
+++ 2.2.py
@@ -1,4 +1,3 @@
-
 c = float(input("temperatura em celcius: "))
 
 print (f"a temperatura em fahrenheit é de {(c * 1.8) + 32}")
---
+++ correção/1.py
@@ -0,0 +1,10 @@
+quantidade = int(input("Insira a quantidade de sandwiches que deseja fazer: "))
+
+presunto_queijo = 0.05
+hamburguer = 0.1
+
+calculo = quantidade * presunto_queijo
+calculo2 = quantidade * hamburguer
+
+print(f"A quantidade para comprar de hamburguer é: {calculo2}Kg")
+print(f"A quantidade para comprar de presunto e queijo é {calculo}Kg")
